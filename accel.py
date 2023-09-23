fc54918beb7c337ea13ada059b9d9bb4d24b3710
JYANG2002 <85294378+JYANG2002@users.noreply.github.com>
2023-09-22 22:42:01 -0400

    Update accel.py

--- accel.py
+++ accel.py
@@ -1,21 +1,45 @@
 import machine
 from time import sleep
 import struct
+import math
+
+def byteUnpack(low, high):
+    low = int.from_bytes(low, 'big')
+    high = int.from_bytes(high, 'big')
+    num = 256 * high + low # Since 2^8 is 256
+    if num >= 32768: # Negatives due to two's comp
+        num = num - 65536
+    return num
+def readAccelerometer(i2c):
+    acc_x = byteUnpack(i2c.readfrom_mem(0x6B, 0x28, 1), i2c.readfrom_mem(0x6B, 0x29, 1)) # Read a byte from OUTX_L_XL & OUTX_H_XL
+    acc_y = byteUnpack(i2c.readfrom_mem(0x6B, 0x2A, 1), i2c.readfrom_mem(0x6B, 0x2B, 1)) # Read a byte from OUTY_L_XL & OUTY_H_XL
+    acc_z = byteUnpack(i2c.readfrom_mem(0x6B, 0x2C, 1), i2c.readfrom_mem(0x6B, 0x2D, 1)) # Read a byte from OUTZ_L_XL & OUTZ_H_XL
+    # Convert to decimal to understand value
+    scale_factor = (2) / (2 ** 15)  # g/LSB
+    g = 9.81 # gravity
+    acc_x *= scale_factor * g
+    acc_y *= scale_factor * g
+    acc_z *= scale_factor * g
+    return [acc_x, acc_y, acc_z]
+
+def calibration(i2c): # Takes 2 seconds to calibrate
+    x = []
+    y = []
+    z = []
+    for i in range(0, 100): # Take in 100 data
+        data = readAccelerometer(i2c)
+        x.append(data[0])
+        y.append(data[1])
+        z.append(data[2])
+        sleep(0.01) # Give time to read since it's 416 Hz so it stabilizes
+    avg_x = sum(x) / 100
+    avg_y = sum(y) / 100
+    avg_z = sum(z) / 100
+    return [avg_x, avg_y, avg_z]
+    
 sda=machine.Pin(16)
 scl=machine.Pin(17)
 i2c=machine.I2C(0,sda=sda, scl=scl, freq=400000)
-
-print('Scan i2c bus...')
-devices = i2c.scan()
-
-if len(devices) == 0:
-    print("No i2c device !")
-else:
-    print('i2c devices found:',len(devices))
-
-for device in devices:
-    print("Decimal address: ",device," | Hexa address: ",hex(device))
-
 # I2C address of the LSM6DS33
 lsm6ds33_address = 0x6B # Use this slave addr for gyroscope and accelerometer
 
@@ -25,9 +49,9 @@ CTRL9_XL_val = 0x38 #
 i2c.writeto_mem(lsm6ds33_address, CTRL9_XL_Reg, bytes([CTRL9_XL_val]))
 
 
-# Acc = 416Hz (High-Performance mode)
+# Acc = 208 Hz (High-Performance mode)
 CTRL1_XL_Reg = 0x10 # Reg ADDR
-CTRL1_XL_val = 0x60
+CTRL1_XL_val = 0x50
 i2c.writeto_mem(lsm6ds33_address, CTRL1_XL_Reg, bytes([CTRL1_XL_val]))
 
 # Acc Data Ready interrupt on INT1
@@ -35,39 +59,35 @@ INT1_CTRL_REG = 0x0D # Reg ADDR
 INT1_CTRL_val = 0x01
 i2c.writeto_mem(lsm6ds33_address, INT1_CTRL_REG, bytes([INT1_CTRL_val]))
 
+# Apply filter
+i2c.writeto_mem(lsm6ds33_address, 0x17, bytes([0x02]))  # 0x02 corresponds to 50Hz LPF, when using CTRL8_XL register
 
-def calibration(i2c, lsm6ds33_address, scale_factor):
-    raw_data = i2c.readfrom_mem(lsm6ds33_address, 0x28, 6) # Output data, each axis takes up 16 bits or 2 bytes
-    x_raw, y_raw, z_raw = struct.unpack('<hhh', raw_data) # Unpack binary data form
-    x_acc = round(x_raw * scale_factor, 1) # Due to the direction chip is facing and the gravity
-    y_acc = round(y_raw * scale_factor, 1)# Make the data into unit of g
-    z_acc = round(z_raw * scale_factor, 1)
-    return [x_acc, y_acc, z_acc]
+print("Calibrating")
+calibrated_data = calibration(i2c)
 
 
-scale_factor = (2.0 * 2) / (2 ** 15)  # g/LSB
-cal_axis = calibration(i2c, lsm6ds33_address, scale_factor)
-x_cal = cal_axis[0]
-y_cal = cal_axis[1]
-z_cal = cal_axis[2]
-x_dis = 0
-y_dis = 0
-z_dis = 0
+x = 0
+y = 0
+z = 0
 while True:
-    raw_data = i2c.readfrom_mem(lsm6ds33_address, 0x28, 6) # Output data, each axis takes up 16 bits or 2 bytes
-    x_raw, y_raw, z_raw = struct.unpack('<hhh', raw_data) # Unpack binary data form
-    # Gives us true acceleration
-    x_acc = round(x_raw * scale_factor, 1) - x_cal 
-    y_acc = round(y_raw * scale_factor, 1) - y_cal
-    z_acc = round(z_raw * scale_factor, 1) - z_cal
-    x_dis += round(0.5 * (x_acc * 9.81), 2) # t^2 doesn't matter because we refresh at t = 1
-    y_dis += round(0.5 * (y_acc * 9.81), 2)
-    z_dis += round(0.5 * (z_acc * 9.81), 2)
-    print("X: " + str(x_dis) + " meters")
-    print("Y: " + str(y_dis) + " meters")
-    print("Z: " + str(z_dis) + " meters")
-        # We find distance using d = 1/2at^2
-    sleep(1)
+    data = readAccelerometer(i2c)
+    # Round to hundredths
+    result = [abs(round(a - b, 2)) for a, b in zip(data, calibrated_data)] # remove abs later
+    # To filter out math errors such as 0.0099999
+    result = [0 if (-0.01 <= value <= -0.009) or (0.009 <= value <= 0.01) else value for value in result]
     
-# Turn raw data into some unit
-# Deal with noise, sensor offset, and gravity to make the measurement as accurate as possible
+    # Print results
+    print("X:", "{:.2f}".format(result[0]), \
+          "| Y:", "{:.2f}".format(result[1]), \
+          "| Z:", "{:.2f}".format(result[2]))
+    # print("X: " + str(result[0])[:3] + " Y: " + str(result[1])[:3] + " Z: " + str(result[2])[:3])
+    """
+    x += result[0] * 0.1 * 100
+    y += result[1] * 0.1 * 100
+    z += result[2] * 0.1 * 100
+    print("X: " + str(x))
+    print("Y: " + str(y))
+    print("Z: " + str(z))
+    """
+
+    sleep(0.1)
